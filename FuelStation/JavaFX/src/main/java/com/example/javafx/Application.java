0d0e7cc7a2d0e0beecb750427dc27b90e71750a9
NahidNouraniVereecke <wi21b089@fh-technikum-wien.at>
2023-05-22 09:16:55 +0200

    Window-size changed

--- FuelStation/JavaFX/src/main/java/com/example/javafx/Application.java
+++ FuelStation/JavaFX/src/main/java/com/example/javafx/Application.java
@@ -10,13 +10,14 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
+
         launch();
     }
+
 }
